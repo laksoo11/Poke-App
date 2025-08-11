35f4bdf2b6b48a2ca17cd75db6c96c6e3e6f6850
Ankit Shakya <ankit.laksoo11@gmail.com>
2025-08-11 21:43:56 +0545

    Added Routing

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,11 +1,13 @@
+import { Link } from "react-router-dom";
+
 
 
 const Nabvar = () => {
     return ( 
         <div>
             <div className="Navbar">
-                <span>Pokemon</span>
-                <h1>HEHE</h1>
+                <Link to = "/">Home</Link>
+                <Link to = "/Pokedex">Pokedex</Link>
             </div>
         </div>
      );
--- src/index.js
+++ src/index.js
@@ -6,19 +6,21 @@ import reportWebVitals from './reportWebVitals';
 import Nabvar from './Navbar';
 import Home from './Home';
 import './navbar.css'
+import Pokedex from './Pokedex';
 
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Nabvar/>
-    <App />
-     <Home />
-     
+   <Router>
+      <Nabvar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/pokedex" element={<Pokedex />} />
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
