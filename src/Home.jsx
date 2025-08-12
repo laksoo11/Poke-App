234bc7d9f46ba4e16a85aac99b286e7467763e0b
Ankit Shakya <ankit.laksoo11@gmail.com>
2025-08-12 19:15:41 +0545

    created useState and implemented

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,12 +1,47 @@
+import { useState } from "react";
+
 const Home = () => {
+
+
+    const [data, SetData] = useState('');
+
+
+    const [name, SetName] = useState('');
+    
+    const HandleSubmit = () => {
+
+        console.log(name);
+
+        fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
+        .then((res) => {
+            return res.json();
+            
+        })
+        .then(data => {
+            
+            SetData(data);
+            console.log(data);
+        
+        });
+
+    }
+
+
+
     return ( 
         <div className="home-container">
             <div>
                 <h1>HOMEPAGE</h1>
                 <p>Welcome to the Pokemon World!!!</p>
+                <button onClick={HandleSubmit}>Search</button>
+                <input value={name} onChange={(e) => SetName(e.target.value)}></input>
+                <h1>{data.name}</h1>
             </div>
         </div>
      );
 }
+
+
+
  
 export default Home;
