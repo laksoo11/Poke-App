3e187b2be49d8b9cabfb917860170bed08394664
Ankit Shakya <ankit.laksoo11@gmail.com>
2025-08-13 23:21:36 +0545

    added error handling while typing pokemon name

--- src/Home.jsx
+++ src/Home.jsx
@@ -7,24 +7,48 @@ const Home = () => {
 
 
     const [name, SetName] = useState('');
+
+    const [error, SetError] = useState('');
+
+    const [loading, SetLoading] = useState(false);
     
     const HandleSubmit = () => {
 
-        console.log(name);
+        SetError("");
+        SetData(null);
+        SetLoading(true);
+
+       
 
         fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
+
+        
         .then((res) => {
+            if (!res.ok) {
+
+          throw new Error("Server not found");
+        }
             return res.json();
             
         })
         .then(data => {
+            // SetLoading('Loading...')
             
             SetData(data);
             console.log(data);
-        
-        });
 
-    }
+        
+        })
+        .catch(() => {
+            
+            SetError("You have entered a wrong Pokémon. Please check the spelling!");
+
+        })
+         .finally(() => {
+        SetLoading(false); // Stop loading
+      });
+
+    };
 
 
 
@@ -33,9 +57,26 @@ const Home = () => {
             <div>
                 <h1>HOMEPAGE</h1>
                 <p>Welcome to the Pokemon World!!!</p>
-                <button onClick={HandleSubmit}>Search</button>
-                <input value={name} onChange={(e) => SetName(e.target.value)}></input>
-                <h1>{data.name}</h1>
+               
+               <input
+          value={name}
+          onChange={(e) => SetName(e.target.value)}
+          placeholder="Enter Pokémon name"
+        />
+        <button onClick={HandleSubmit}>Search</button>
+
+        <div></div>
+
+        {loading && <p style={{ color: "blue" }}>{loading}</p>}
+
+        {error && <p style={{ color: "red" }}>{error}</p>}
+
+        
+
+        
+
+        {data && <h1>{data.name}</h1>}
+
             </div>
         </div>
      );
